cda5feb52006535f2f998323685d89ba68a62d85
swopnil123 <swopnilojha@gmail.com>
2017-08-04 14:20:49 +0545

    Update Real_spectra.py

--- Real_spectra.py
+++ Real_spectra.py
@@ -16,7 +16,7 @@ style.use('ggplot')
 
 from numba import jit 
 
-"""This code reads earthquake ground motion data from folder and generates
+"""This code reads earthquake ground motion data (PEER Format) from folder and generates
 elastic resposne spectra and computes the Median sa and Stdlog(sa) for 
 the ensemble
 """
